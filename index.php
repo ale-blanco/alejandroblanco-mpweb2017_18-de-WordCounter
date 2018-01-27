b8caef0bb9392f0028a9771d9a1904475bf4f524
alejandroblanco <alejandroblancoluna@gmail.com>
2018-01-27 11:13:57 +0100

    Añadida opcion de encadenar filtros mediante decoradores

--- index.php
+++ index.php
@@ -15,8 +15,19 @@ define(
 $counter = new Counter(TEXT_TO_COUNT);
 echo 'Palabras totales: ' . $counter->getCountOfWord(null) . PHP_EOL;
 
-echo 'Palabras que empiecen por vocal: ' . $counter->getCountOfWord(new FirstCharVowel()) . PHP_EOL;
 
-echo 'Palabras de más de dos caracteres: ' . $counter->getCountOfWord(new MoreTowChars()) . PHP_EOL;
+echo 'Palabras que empiecen por vocal: ' . $counter->getCountOfWord(new FirstCharVowel(null)) . PHP_EOL;
 
-echo 'En listado de palabras clave: ' . $counter->getCountOfWord(new WordInTheList()) . PHP_EOL;
+echo 'Palabras de más de dos caracteres: ' . $counter->getCountOfWord(new MoreTowChars(null)) . PHP_EOL;
+
+echo 'En listado de palabras clave: ' . $counter->getCountOfWord(new WordInTheList(null)) . PHP_EOL;
+
+
+echo 'Empiecen por vocal y tengan más de 2 caracteres: '
+    . $counter->getCountOfWord(new FirstCharVowel(new MoreTowChars(null))) . PHP_EOL;
+
+echo 'En listado de palabras clave que empiecen por vocal: '
+    . $counter->getCountOfWord(new WordInTheList(new FirstCharVowel(null))) . PHP_EOL;
+
+echo 'En listado de palabras clave que empiecen por vocal y tengan más de dos carácteres: '
+    . $counter->getCountOfWord(new WordInTheList(new FirstCharVowel(new MoreTowChars(null)))) . PHP_EOL;
--- src/WordCounter/Filters/Filter.php
+++ src/WordCounter/Filters/Filter.php
@@ -4,10 +4,20 @@ namespace WordCounter\Filters;
 
 abstract class Filter
 {
+    private $previousFilter;
+
+    public function __construct(?Filter $previousFilter)
+    {
+        $this->previousFilter = $previousFilter;
+    }
+
     protected abstract function isValidWord(string $word): bool;
 
     public function filterWords(array $wordsList): array
     {
-        return array_filter($wordsList, [$this, 'isValidWord']);
+        $preFilterList = ($this->previousFilter !== null)
+            ? $this->previousFilter->filterWords($wordsList)
+            : $wordsList;
+        return array_filter($preFilterList, [$this, 'isValidWord']);
     }
 }
